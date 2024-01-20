74b46ff4f2d1ea369f369dbde4182764351f12c9
0-mkdad <mohammad.almekdad@hotmail.com>
2024-01-20 17:50:41 +0100

    Add info page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Homepage } from "./components/homepage/homepage";
+import { Infopage } from "./components/infopage/infopage";
 
 function App() {
   return (
     <>
       <Homepage></Homepage>
+      <Infopage></Infopage>
     </>
   );
 }
